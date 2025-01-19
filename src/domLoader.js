3d334df1c2dd111505137f779bf45743bdc3410c
Javier <J_sedanomar@uncg.edu>
2025-01-19 16:01:45 -0500

    completed the reservation section, need to add styling to it

--- src/domLoader.js
+++ src/domLoader.js
@@ -17,6 +17,16 @@ mainDescription.classList.add('section1', 'main-description');
 const reservationInfo = document.createElement('div');
 reservationInfo.classList.add('section2', 'reservation');
 
+const reserveHeading = document.createElement('h2')
+reserveHeading.textContent = 'Reservation';
+const reserveSubHeading = document.createElement('h5')
+reserveSubHeading.textContent = 'We Look Forward To Hosting You';
+const reserveBody = document.createElement('p');
+reserveBody.textContent = "Join us at Pelu Pasta and reserve your table for an authentic Italian dining experience! Whether you're celebrating a special occasion or enjoying a casual meal with friends, reserving in advance guarantees a spot for you to indulge in our delicious pasta and exceptional service.";
+const reserveButton = document.createElement('button');
+reserveButton.textContent = 'Reserve';
+reservationInfo.append(reserveHeading,reserveSubHeading,reserveBody,reserveButton);
+
 
 const cateringInfo = document.createElement('div');
 cateringInfo.classList.add('section1', 'catering');
