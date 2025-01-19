40c3ea86caae3175222be592f9d5a1ddf7c4cf7a
Javier <J_sedanomar@uncg.edu>
2025-01-19 15:22:45 -0500

    created the sections in dom Loader that i'm gonig to add to parent and gave them classes for dom manipulation

---
+++ src/domLoader.js
@@ -0,0 +1,29 @@
+const parentContainer = document.getElementById('content');
+
+
+const heroContainer = document.createElement('div');
+heroContainer.classList.add('section1', 'hero-container');
+
+
+const contactInfo = document.createElement('div');
+contactInfo.classList.add('section2', 'contact-info');
+
+
+const mainDescription = document.createElement('div');
+mainDescription.classList.add('section1', 'main-description');
+
+
+const reservationInfo = document.createElement('div');
+reservationInfo.classList.add('section2', 'reservation');
+
+
+const cateringInfo = document.createElement('div');
+cateringInfo.classList.add('section1', 'catering');
+
+
+const reviews = document.createElement('div');
+reviews.classList.add('section2', 'reviews');
+
+
+const hiringInfo = document.createElement('div');
+hiringInfo.classList.add('section1', 'hiring');
