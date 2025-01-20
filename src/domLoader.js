2702602944bd1b4f811ea4f34fef45048f55ae2e
Javier <J_sedanomar@uncg.edu>
2025-01-19 20:38:33 -0500

    finished content for catering area

--- src/domLoader.js
+++ src/domLoader.js
@@ -57,14 +57,18 @@ function createTextBox(title, subtitle, bodyText, buttonTitle){
 
 const cateringInfo = document.createElement('div');
 cateringInfo.classList.add('section1', 'catering');
+
 const image1Catering = document.createElement('img');
-const textBox1Catering = document.createElement('div');
-const textForTextBox1 = "Savor the taste of Italy with Pelu Pasta! Order your favorite authentic Italian dishes online and enjoy a delicious meal in the comfort of your own home. With our simple online ordering system, you can explore our menu, customize your pasta, and choose pickup or delivery in just a few clicks. Your Italian culinary journey starts here"
-const textBox1 = createTextBox("Order Online", "Take Us Home With You", textForTextBox1, "Order")
+const textForTextBox1 = "Savor the taste of Italy with Pelu Pasta! Order your favorite authentic Italian dishes online and enjoy a delicious meal in the comfort of your own home. With our simple online ordering system, you can explore our menu, customize your pasta, and choose pickup or delivery in just a few clicks. Your Italian culinary journey starts here";
+const textBox1Catering = createTextBox("Order Online", "Take Us Home With You", textForTextBox1, "Order")
+
 const image2Catering = document.createElement('img');
-const textBox2Catering = document.createElement('div');
+const textForTextBox2 = "Make your event unforgettable with catering from Pelu Pasta! Our talented team will craft a personalized menu showcasing our signature pasta dishes and authentic Italian flavors, ensuring a delightful dining experience that will impress you and your guests. Let us bring the taste of Italy to your special occasion!";
+const textBox2Catering = createTextBox("CATERING", "Catering Made Easy", textForTextBox2, "Catering");
+
 const image3Catering = document.createElement('img');
-const textBox3Catering = document.createElement('div')
+const textForTextBox3 = "Celebrate in style at Pelu Pasta with a private party designed just for you! With our cozy ambiance, exceptional service, and delectable Italian pasta dishes, your event will be a memorable experience that guests of all ages will enjoy. Let us make your special occasion truly unforgettable!";
+const textBox3Catering = createTextBox("Groups & Parties", "We Welcome Every Type Of Private Event", textForTextBox3, "Parties");
 
 
 const reviews = document.createElement('div');
